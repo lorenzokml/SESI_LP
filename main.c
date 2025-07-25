7421e0f2b5829721a83895d048ba37852ec95a1d
lorenzo <lorenzodev2009@gmail.com>
2025-07-25 10:52:12 -0300

    meu primeiro código

---
+++ main.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World \n");
+    int n1 = 10;
+    int n2 = 20;
+    int s = n1+n2;
+    printf("A soma dos valores é: %d",s);
+
+}
